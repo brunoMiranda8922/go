c54f3196d1204803f99b121e75f332f9e4beee31
Bruno Miranda <bruno.silva@catho.com>
2018-08-18 02:01:21 -0300

    TypeOf/Array/Slice

--- hello.go
+++ hello.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"time"
 )
 
 func main() {
@@ -11,15 +12,30 @@ func main() {
 	fmt.Println("Mensagem é do tipo:", reflect.TypeOf(mensagem))
 	fmt.Println("Digite um número entre 1 á 10")
 
+	//caminho, posição na mémoria que a variavel esta
+
 	var numero int
 	fmt.Scan(&numero)
-	fmt.Println(&numero)
+	fmt.Println("Posição na memória é:", &numero)
+
+	//array
 
 	var array [4]int
 	array[0] = 22
 	array[1] = numero
 	array[2] = 0
 	array[3] = array[0]
-
 	fmt.Println("Retorno do array", array)
+	fmt.Println("Tamanho do array:", len(array))
+	fmt.Println("Capacidade do array é:", cap(array))
+	//Slice -> abstração de um array
+
+	slice := []string{"Bruno", "Marcio", "Barreto"}
+	fmt.Println("Retorno do slice:", slice, "Tamanho do slice é de ", len(slice))
+	fmt.Println("Capacidade do slice é:", cap(slice))
+	fmt.Println("Adicionando mais um elemento no slice..")
+	time.Sleep(3000 * time.Millisecond)
+	slice = append(slice, "Mario")
+	fmt.Println("Retorno do slice:", slice, "Tamanho do slice é de ", len(slice))
+	fmt.Println("Capacidade do slice é:", cap(slice))
 }
