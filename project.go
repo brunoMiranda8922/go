16fb4b7c323c064f98e786bbe4a8bfa5e9795e3e
Bruno Miranda <bruno.silva@catho.com>
2018-08-18 02:47:16 -0300

    Monitorando alguns sites

--- project.go
+++ project.go
@@ -4,24 +4,29 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	exibirDono()
-	exibirMenu()
-	comando := lerComando()
+	for {
+		exibirMenu()
+		comando := lerComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando..")
-		iniciarMonitoramento()
-	case 2:
-		fmt.Println("Logs..")
-	case 0:
-		fmt.Println("Saindo..")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando inválido")
+		switch comando {
+		case 1:
+			fmt.Println("Monitorando..")
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Logs..")
+		case 0:
+			fmt.Println("Saindo..")
+			time.Sleep(2000 * time.Millisecond)
+			os.Exit(0)
+
+		default:
+			fmt.Println("Comando inválido")
+		}
 	}
 }
 
@@ -44,8 +49,23 @@ func lerComando() int {
 }
 
 func iniciarMonitoramento() {
-	site := "http://www.catho.com"
-	resp, _ := http.Get(site)
-	fmt.Println(resp)
+	sites := []string{"https://random-status-code.herokuapp.com/",
+		"https://www.catho.com.br", "http://www.fatecbarueri.edu.br/"}
 
+	for i, site := range sites {
+		fmt.Println("Testando site", i+1)
+		time.Sleep(1000 * time.Millisecond)
+		verificarSite(site)
+	}
+
+}
+
+func verificarSite(site string) {
+
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregando com sucesso. status:", resp.StatusCode)
+	} else {
+		fmt.Println("Site:", site, "Apresentou algum erro. status:", resp.StatusCode)
+	}
 }
